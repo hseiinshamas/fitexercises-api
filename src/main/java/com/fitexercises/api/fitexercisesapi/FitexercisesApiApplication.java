df2bdb5d8fe92d127b13214b685f9963c12086fa
Hussein Shamas <hssenshamas@gmail.com>
2023-09-03 17:08:19 +0300

    Added a in-memory user details service for testing, and studying purposes (spring security boot starter)

--- src/main/java/com/fitexercises/api/fitexercisesapi/FitexercisesApiApplication.java
+++ src/main/java/com/fitexercises/api/fitexercisesapi/FitexercisesApiApplication.java
@@ -7,9 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 
 public class FitexercisesApiApplication {
-
     public static void main(String[] args) {
+
         SpringApplication.run(FitexercisesApiApplication.class, args);
+
     }
 
 }
